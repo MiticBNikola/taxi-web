0a9fbb432788dae9301b19fee5ba88b7902b3049
Nikola Mitic <n.mitic97@gmail.com>
2022-07-17 20:15:11 +0200

    Restyle home screen

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {HomeComponent} from './home.component';
+import {MatIconModule} from '@angular/material/icon';
 
 
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [HomeComponent],
+    imports: [
+        CommonModule,
+        MatIconModule
+    ]
 })
 export class HomeModule { }

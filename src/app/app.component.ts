7aff2fba19d38d6b40b1d76966e43182f1d88bd0
NikolaMitic-Softelm <nikola.mitic@softelm.com>
2024-09-29 04:11:49 +0200

    feat: add toasts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,12 @@ import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './_layout/footer/footer.component';
 import { HeaderComponent } from './_layout/header/header.component';
 import { NavComponent } from './_layout/nav/nav.component';
+import { ToastComponent } from './_shared/components/toast/toast.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FooterComponent, NgSwitch, HeaderComponent, NavComponent],
+  imports: [RouterOutlet, FooterComponent, NgSwitch, HeaderComponent, NavComponent, ToastComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })

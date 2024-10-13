4c5acac1b101edc5796de900353315a3fe696323
Nikola Mitic <mitic.nikola@elfak.rs>
2024-10-13 14:08:57 +0200

    add loader handler

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { NgSwitch } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { FooterComponent } from './_layout/footer/footer.component';
 import { HeaderComponent } from './_layout/header/header.component';
 import { NavComponent } from './_layout/nav/nav.component';
 import { ToastComponent } from './_shared/components/toast/toast.component';
+import { ToastService } from './_shared/services/toast.service';
 
 import { environment } from '../environments/environment';
 
@@ -16,12 +17,46 @@ import { environment } from '../environments/environment';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
-  title = 'taxi-web';
+export class AppComponent {
+  private toastService = inject(ToastService);
 
-  ngOnInit() {
-    const script = document.createElement('script');
-    script.src = `https://maps.googleapis.com/maps/api/js?key=${environment.GOOGLE_API_KEY}&loading=async&libraries=places&v=weekly`;
-    document.body.appendChild(script);
+  title = 'taxi-web';
+  googleMapsFullyLoaded = false;
+
+  constructor() {
+    this.loadGoogleMaps();
+  }
+
+  loadGoogleMaps(): Promise<void> {
+    if (this.googleMapsFullyLoaded) {
+      return Promise.resolve();
+    }
+    return new Promise((resolve, reject) => {
+      const script = document.createElement('script');
+      script.src = `https://maps.googleapis.com/maps/api/js?key=${environment.GOOGLE_API_KEY}&loading=async&libraries=places&v=weekly`;
+      document.body.appendChild(script);
+      script.onload = () => {
+        const checkGoogleMaps = setInterval(() => {
+          if (
+            typeof google !== 'undefined' &&
+            google.maps &&
+            google.maps.places &&
+            google.maps.Geocoder &&
+            google.maps.DirectionsService &&
+            google.maps.LatLng
+          ) {
+            clearInterval(checkGoogleMaps);
+            this.googleMapsFullyLoaded = true;
+          } else {
+            console.error('Waiting for Google Maps to load...');
+          }
+        }, 50);
+        resolve();
+      };
+      script.onerror = (error) => {
+        this.toastService.error('Google Maps ne funkcioniše. Probajte ponovo kasnije!');
+        reject(error);
+      };
+    });
   }
 }

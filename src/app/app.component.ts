9674500df14e4d7b571ad18666c3cdfe88c3b556
Nikola Mitic <mitic.nikola@elfak.rs>
2024-10-13 12:59:27 +0200

    move google script load on init of app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { NgSwitch } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { FooterComponent } from './_layout/footer/footer.component';
 import { HeaderComponent } from './_layout/header/header.component';
 import { NavComponent } from './_layout/nav/nav.component';
 import { ToastComponent } from './_shared/components/toast/toast.component';
+
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -14,6 +16,12 @@ import { ToastComponent } from './_shared/components/toast/toast.component';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'taxi-web';
+
+  ngOnInit() {
+    const script = document.createElement('script');
+    script.src = `https://maps.googleapis.com/maps/api/js?key=${environment.GOOGLE_API_KEY}&loading=async&libraries=places&v=weekly`;
+    document.body.appendChild(script);
+  }
 }
